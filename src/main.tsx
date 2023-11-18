d896b803319fd1ef28e1aed3a348b8fcaff64326
YuriPC <yuriprogammer@gmail.com>
2023-11-18 19:49:14 -0300

    navbar working!

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import IndexNavbar from './components/navbar/IndexNavbar.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <IndexNavbar/>
   </React.StrictMode>,
 )
