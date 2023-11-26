eec93c7c950de3318a37c1605c1263f3343a651e
YuriPC <yuriprogammer@gmail.com>
2023-11-26 15:44:32 -0300

    interface rowdata

--- src/views/CGVN.tsx
+++ src/views/CGVN.tsx
@@ -1,21 +1,25 @@
 import { useState } from "react";
-
 import { AgGridReact } from "ag-grid-react";
-
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
+interface RowData {
+  make: string;
+  model: string;
+  price: number;
+}
+
 export default function CGVS() {
-  const [rowData] = useState([
+  const [rowData] = useState<RowData[]>([
     { make: "Toyota", model: "Celica", price: 35000 },
     { make: "Ford", model: "Mondeo", price: 32000 },
     { make: "Porsche", model: "Boxter", price: 72000 }
   ]);
 
   const [columnDefs] = useState([
-    { field: "make" },
-    { field: "model" },
-    { field: "price" }
+    { headerName: "Make", field: "make" },
+    { headerName: "Model", field: "model" },
+    { headerName: "Price", field: "price" }
   ]);
 
   return (
