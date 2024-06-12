ac9c56d3ba131ddbe3a2f376f9a76f9e94d5f9fd
unknown <yuriprogammer@gmail.com>
2024-06-11 21:31:26 -0300

    nada

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -20,14 +20,14 @@ export default function Home() {
           <Row className="h-100 justify-content-center align-items-center">
             <Col md={6} className="text-center text-white">
               <h1>Welcome to Marcos's Website</h1>
-              <h2>Professor of UFSJ</h2>
+              <h2>Professor at UFSJ</h2>
             </Col>
           </Row>
         </Container>
       </div>
       <Row className="justify-content-md-center">
         <Col xs={12} md={6} className="text-center">
-          <h1>Welcome to My Website</h1>
+          <h1>Welcome!</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquet
             sem id risus posuere, vel mattis arcu viverra. Donec finibus
